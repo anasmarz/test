d8716507960a627f64c2789b4435de6c78a84fb9
AnasUpm <muhammadanasmarz@gmail.com>
2024-07-16 22:27:06 +0800

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import '../styles/Home.css';
+
 export default function Home() {
+  const handleButtonClick = (message) => {
+    alert(message);
+  };
+
   return (
     <div className="home">
       <h1>Welcome to the Home Page</h1>
       <p>Haziq is the gayest man alive</p>
 
       <div>
-        <p>This is a website dedicated for her</p>
+        <p>This is a website dedicated for her </p>
+        <div className="fixToText">
+          <button onClick={() => handleButtonClick('Left button pressed')}>
+            Left button
+          </button>
+          <button onClick={() => handleButtonClick('Right button pressed')}>
+            Right button
+          </button>
+        </div>
+
+        <button>click</button>
       </div>
-
-
     </div>
   );
 }
